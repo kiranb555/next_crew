d57e3ea7cd23554aef1c12a8281fe061be84ca38
kiranb555 <kiran.basavaraj05@gmail.com>
2025-07-06 20:50:24 +0530

    build changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-export default{
+export default {
   output: "export",
-  images: { unoptimized: true }, // If you use next/image
-  basePath: "/next_crew", // Change to your actual repo name, e.g. /next_crew
-  assetPrefix: "/next_crew/", // Change to your actual repo name, e.g. /next_crew/
+  images: { unoptimized: true },
+  basePath: "/next_crew",
+  assetPrefix: "/next_crew/",
 };
