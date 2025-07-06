6592e61a28922a7095c2b502723392e59f357407
kiranb555 <kiran.basavaraj05@gmail.com>
2025-07-06 20:19:22 +0530

    build changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default{
+  output: "export",
+  images: { unoptimized: true }, // If you use next/image
+  basePath: "/next_crew", // Change to your actual repo name, e.g. /next_crew
+  assetPrefix: "/next_crew/", // Change to your actual repo name, e.g. /next_crew/
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src * data:; script-src 'self' 'unsafe-inline';"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Header />
         {children}
         <Footer />
